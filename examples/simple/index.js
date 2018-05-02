f0604e70a8e2af83ba3811a2265e07f3c2475a28
DESKTOP-R9K5VO5\admin <jeremy_croff@live.nl>
2018-05-02 00:37:56 -0500

    updating bart

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -2,15 +2,14 @@ import util from 'util';
 
 import server from '../../server';
 import { seed, setDefinitions, setLoggingLevel } from '../../src/main';
-import personas from './personas';
+import { bartSimpson } from './personas';
 import definitions from './definitions';
 
 
 server.ready.then(() => {
-  const persona = personas.bart_simpson;
   setLoggingLevel('info');
   setDefinitions(definitions);
-  seed(persona).then((output) => {
+  seed(bartSimpson).then((output) => {
     console.log('Output data', util.inspect(output, false, 20, true)); // eslint-disable-line no-console
     process.exit();
   });
