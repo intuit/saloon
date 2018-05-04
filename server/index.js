398ebf5ce49e55b748eba5d1f7c59e4457c003da
Croff <jcroff@PLNL160adc342.local>
2018-05-03 23:17:47 -0700

    lint update for code review comment

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-// eslint-disable import/no-extraneous-dependencies
+/* eslint-env node */
+
 import restify from 'restify'; // eslint-disable-line import/no-extraneous-dependencies
 
 /**
@@ -26,24 +27,45 @@ const router = (server) => {
 
   server.post('/api/firm', (req, res, next) => {
     const newFirm = req.params;
+
+    if (!users.find(user => user.user_id === newFirm.user_id)) {
+      res.send(400, { error: 'User not found' });
+      return next();
+    }
+
     newFirm.firm_id = '321';
     firms.push(newFirm);
+
     res.send(201, newFirm);
     return next();
   });
 
   server.post('/api/client', (req, res, next) => {
     const newClient = req.params;
+
+    if (!firms.find(firm => firm.firm_id === newClient.firm_id)) {
+      res.send(400, { error: 'Firm not found' });
+      return next();
+    }
+
     newClient.client_id = '213';
     clients.push(newClient);
+
     res.send(201, newClient);
     return next();
   });
 
   server.post('/api/taxreturn', (req, res, next) => {
     const newReturn = req.params;
+
+    if (!clients.find(client => client.client_id === newReturn.client_id)) {
+      res.send(400, { error: 'Client not found' });
+      return next();
+    }
+
     newReturn.return_id = '3124';
     returns.push(newReturn);
+
     res.send(201, newReturn);
     return next();
   });
