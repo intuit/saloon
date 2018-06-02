889d076a8bb4a4147f9dc95e9fe1ca3268667e9a
Wilson, James <James_Wilson@intuit.com>
2018-06-01 20:35:56 -0500

    Temporarily removing winston

--- src/logger.js
+++ src/logger.js
@@ -1,82 +1,23 @@
-import winston from 'winston';
 
 /**
  * A logger utility.
  * @class
  */
 class Logger {
-  constructor() {
-    /**
-         * The default logging configuration.
-         * @type {Object}
-         * @access private
-         */
-    this._defaultConfig = {
-      levels: {
-        verbose: 1,
-        debug: 2,
-        info: 3,
-        warn: 4,
-        error: 5,
-      },
-      colors: {
-        verbose: 'grey',
-        debug: 'white',
-        info: 'cyan',
-        warn: 'yellow',
-        error: 'red',
-      },
-    };
-
-    /**
-         * The default console transport.
-         * @type {Object}
-         * @access private
-         */
-    this._consoleTransport = new winston.transports.Console({
-      timestamp: true,
-      json: false,
-      colorize: true,
-      handleExceptions: true,
-      level: 'warn',
-    });
-
-    /**
-         * The logger instance.
-         * @type {Object}
-         * @access private
-         */
-    this._logger = new winston.Logger({
-      transports: [this._consoleTransport],
-      levels: this._defaultConfig.levels,
-      colors: this._defaultConfig.colors,
-    });
-  }
-
   /**
      * A way to set a custom logging level.
-     * @param {string} level The logging level to set (debug, info, error, etc).
      * @access public
      */
-  setLoggingLevel(level) {
-    this._logger.configure({
-      level,
-      transports: [this._consoleTransport],
-    });
+  setLoggingLevel() {
+    this.warn('Setting logging level is currently disabled.');
   }
 
   /**
      * Enable logging to file.
-     * @param {string} filename
      * @access public
      */
-  logToFile(filename) {
-    this._logger.configure({
-      transports: [
-        this._consoleTransport,
-        new winston.transports.File({ filename }),
-      ],
-    });
+  logToFile() {
+    this.warn('Logging to file is currently disabled.');
   }
 
   /**
@@ -122,7 +63,14 @@ class Logger {
      * @access private
      */
   _log(level, msg) {
-    this._logger.log(level, msg);
+    let output = msg;
+
+    if (typeof output === 'object') {
+      output = JSON.stringify(output);
+    }
+
+    output = `${new Date().toUTCString()} ${level}: ${output}`;
+    console[level].call(this, output); // eslint-disable-line no-console
   }
 }
 
